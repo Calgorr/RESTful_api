a4345073ebb64b02bde554e59b29ce09dc3b73ef
Calgor <ahomayobi684@gmail.com>
2022-09-05 17:35:10 +0430

    3 endpoints

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbmusById)
 	router.Run("localhost:8080")
 }
 
@@ -38,4 +39,15 @@ func postAlbums(c *gin.Context) {
 	}
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusOK, newAlbum)
+}
+
+func getAlbmusById(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
